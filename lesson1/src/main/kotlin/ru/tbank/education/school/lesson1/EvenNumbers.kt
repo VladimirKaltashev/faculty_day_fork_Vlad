de519a9932912fc907e69f6f4043828197e1c933
VladimirKaltashev <107103795+VladimirKaltashev@users.noreply.github.com>
2025-11-06 14:49:15 +0300

    Функция суммы четных чисел массива

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
@@ -3,6 +3,18 @@ package ru.tbank.education.school.lesson1
 /**
  * Сумма четных чисел.
  */
-fun sumEvenNumbers(numbers: Array<Int>): Int {
-    TODO()
+fun sumEvenNumbers(numbers: Array<Int>) =
+    numbers.filter { it % 2 == 0}.sum()
+
+fun main() {
+    val numbers = arrayOf(1, 2, 3, 4, 5, 6)
+    val sumEven = sumEvenNumbers(numbers)
+    println(sumEven)
 }
+//fun sumEvenNumbers(numbers: Array<Int>): Int {
+//    var sum = 0
+//    for (elem in numbers) {
+//        if (elem % 2 == 0) sum += elem
+//    }
+//    return sum
+//}
