982caaf7321b5b76ec7c34c417f09d9cafad8833
VladimirKaltashev <107103795+VladimirKaltashev@users.noreply.github.com>
2025-11-06 10:48:54 +0300

    Изменено содержание main()

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
@@ -1,5 +1,12 @@
 package ru.tbank.education.school.lesson1
 
 fun main() {
-    println("Hello World!")
+    var num = 1
+    var str1 = "before: $num"
+    println(str1)
+    num++
+    var str2 = "after: $num"
+    println(str2)
+    var b: Short? = null
+    println(b)
 }
