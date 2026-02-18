978346f37de58e57b7c8bbce4fe25e59e76748ab
VladimirKaltashev <107103795+VladimirKaltashev@users.noreply.github.com>
2026-02-18 22:27:19 +0300

    gradle settings

--- lesson10/build.gradle.kts
+++ lesson10/build.gradle.kts
@@ -4,3 +4,15 @@ plugins {
 dependencies {
     testImplementation(kotlin("test"))
 }
+
+tasks.withType<JavaExec>().configureEach {
+    jvmArgs(
+        "-Dfile.encoding=UTF-8",
+        "-Dsun.stdout.encoding=UTF-8",
+        "-Dsun.stderr.encoding=UTF-8"
+    )
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+}
